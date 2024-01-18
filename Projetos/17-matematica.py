f6ef72c300a2c49e32b2e1283624f2506d7a9bd5
Renato Miranda Setúbal <124818941+rmset@users.noreply.github.com>
2024-01-18 08:52:51 -0300

    Inclusão de métodos default de matemática

---
+++ Projetos/17-matematica.py
@@ -0,0 +1,21 @@
+
+
+#Funções builtin
+#São funções que já existem  por padrão no python e não é necessário a importação de módulos
+valores = [2,3,4,6,-6,-9,-7]
+
+print(max(valores)) #Vai verificar qual é o maior número na lista
+print(min(valores)) #Vai verificar qual é o menor número na lista
+
+#Tratando valor Absoluto
+a = -5
+b = 6
+print(abs(a)) #Deve retornar o valor 5
+
+print(pow(2,3)) #Exponenciação
+
+#Função de arrendondamento
+c = 2.7834
+print(round(c,2)) #Primeiro parâmetro é o número, segundo parâmetro as casas decimais. 
+
+
