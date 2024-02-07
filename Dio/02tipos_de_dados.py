febe721c483a99538ec0941d88bcabd5e3f48233
Renato Miranda Setúbal <124818941+rmset@users.noreply.github.com>
2024-02-07 09:06:37 -0300

    Inclusão dos exemplos de conversão de dados.

---
+++ Dio/02tipos_de_dados.py
@@ -0,0 +1,7 @@
+#Inteiro
+print((1+10)*2) 
+#Float
+print(2.3 + 3.2)
+#Boolean
+print(True)
+print(False)
---
+++ Dio/03-conversao_de_tipos.py
@@ -0,0 +1,9 @@
+age = 42
+name = "Renato Miranda Setúbal"
+print(f'Meu nome é {name} e minha idade é {age}')
+
+
+preco=float(42)
+print(float(200))
+print(int(10.5))
+print(str(10.50))
