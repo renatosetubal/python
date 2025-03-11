7a9717be3606c0e5d849521955f9c382b4430190
Renato Miranda Setubal <remiset@gmail.com>
2025-03-11 11:18:39 -0300

    Update:Continuação Flet

--- Curso2/12-flet/07.1-elevated_button.py
+++ Curso2/12-flet/07.1-elevated_button.py
@@ -16,6 +16,32 @@ def main(page:ft.Page):
     b = ft.ElevatedButton("Botão Clicado", on_click=button_clicked, data=0)
     t = ft.Text()
     page.add(b,t)
+
+    page.add(
+        ft.ElevatedButton(
+            width=150,
+            content=ft.Row(
+                [
+                   ft.Icon(name=ft.icons.FAVORITE, color="yellow"), 
+                   ft.Icon(name=ft.icons.AUDIOTRACK, color="blue"), 
+                   ft.Icon(name=ft.icons.BEACH_ACCESS, color="green"), 
+                ],
+            ),
+        ),
+        ft.ElevatedButton(
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.Text(value="Botão Composto", size=20),
+                        ft.Text(value="Texto Secundário", size=20),
+                    ], 
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    spacing=5
+                ),
+                padding=ft.padding.all(10)
+            )
+        )
+    )
     
     page.update()
 ft.app(target=main)
